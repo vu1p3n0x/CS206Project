6578cc06f4c026d41cd4140a0970f1f6a61fd221
Brad Trapp <bjthdc@gmail.com>
2013-04-09 20:15:33 -0500

    Added Classes
    Added basic Card and Settings classes to the CS206Project

---
+++ CS206Project/CS206Project/CS206Project/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CS206Project
+{
+    class Card
+    {
+        public int number;
+        enum suits { Spades, Hearts, Diamonds, Clubs };
+        public suits cardSuit;
+        public bool visible;
+
+        public Card(int newNumber, suits newSuit)
+        {
+            number = newNumber;
+            cardSuit = newSuit;
+        }
+
+    }
+}
---
+++ CS206Project/CS206Project/CS206Project/Settings.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CS206Project
+{
+    class Settings
+    {
+        int numCards;
+        int maxPlayers;
+        string playerName;
+        int currentCardBack;
+        int currentBack;
+
+        public Settings(){
+            numCards = 8;
+            maxPlayers = 4;
+            playerName = "Bob";
+            currentCardBack = 1;
+            currentBack = 1;
+        }
+
+        private int getNumCards()
+        {
+            return numCards;
+        }
+        
+        private int getMaxPlayers()
+        {
+            return maxPlayers;
+        }
+
+        private string getPlayerName()
+        {
+            return playerName;
+        }
+
+        private int getCurrentCardBack()
+        {
+            return currentCardBack;
+        }
+
+        private int getCurrentBack()
+        {
+            return currentBack;
+        }
+
+        private void setNumCards(int n)
+        {
+            numCards = n;
+        }
+
+        private void setMaxPlayers(int n)
+        {
+            maxPlayers = n;
+        }
+
+        private void setPlayerName(string name)
+        {
+            playerName = name;
+        }
+
+        private void setCurrentCardBack(int n)
+        {
+            currentCardBack = n;
+        }
+
+        private void setCurrentBack(int n)
+        {
+            currentBack = n;
+        }
+    }
+}
