1156f6e459f57a4b406241d41ff1005fff597b5a
Brad Trapp <bjthdc@gmail.com>
2013-04-16 14:44:53 -0500

    another problem

---
+++ CS206Project/CS206Project/CS206Project/MainScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS206Project
+{
+    class MainScreen : Screen
+    {
+        public override bool Initialize(Game1 game)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool LoadContent(Game1 game)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Update(Game1 game, Microsoft.Xna.Framework.GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Draw(Game1 game, Microsoft.Xna.Framework.GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool HasNextScreen()
+        {
+            throw new NotImplementedException();
+        }
+        public override Screen GetNextScreen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
