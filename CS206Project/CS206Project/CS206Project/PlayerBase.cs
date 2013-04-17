b68290a9dc888aea9070bedd8a4a62bff486c9e5
Trevor Wilson <vu1p3n0x@gmail.com>
2013-04-17 15:20:04 -0500

    Added PlayerBase class
    To be used to define types of players (User vs AI)

---
+++ CS206Project/CS206Project/CS206Project/PlayerBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace CS206Project
+{
+    abstract class PlayerBase
+    {
+        // constructors and destructors
+        public PlayerBase()
+        {
+
+        }
+        ~PlayerBase()
+        {
+
+        }
+
+        // basic override functions
+        public abstract bool Initialize(Game1 game);
+        public abstract bool LoadContent(Game1 game);
+        public abstract bool Update(Game1 game, GameTime time);
+        public abstract bool Draw(Game1 game, GameTime time);
+    }
+}
