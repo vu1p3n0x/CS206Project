f561cd4c72dd9657bc5a4670b7be01765ad864b4
Brad Trapp <bjthdc@gmail.com>
2013-04-18 14:34:48 -0500

    Fixed Merger

---
+++ CS206Project/CS206Project/CS206Project/OptionScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CS206Project
+{
+    class OptionScreen : Screen
+    {
+        Texture2D pixel;
+
+        // constructors and destructors
+        public OptionScreen()
+        {
+
+        }
+        ~OptionScreen()
+        {
+
+        }
+
+        // base override functions
+        public override bool Initialize(Game1 game)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool LoadContent(Game1 game)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Update(Game1 game, Microsoft.Xna.Framework.GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Draw(Game1 game, Microsoft.Xna.Framework.GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
