c8fd2b5c0b4284dd3d068ffeb61f247db5c4074a
Trevor Wilson <vu1p3n0x@gmail.com>
2013-04-25 14:46:05 -0500

    A change occured

--- CS206Project/CS206Project/CS206Project/GameScreen.cs
+++ CS206Project/CS206Project/CS206Project/GameScreen.cs
@@ -106,7 +106,9 @@ namespace CS206Project
         {
             //Draws background
             game.spriteBatch.Draw(table,background,Color.White);
+
             //Draws player 0's (user) field
+            // game.spriteBatch.Draw(game.settings.images, fields[0, 0], game.settings.cardBacks[players[0].field[0].getSuit(), players[0].field[0].getNumber()], Color.White);
             game.spriteBatch.Draw(pixel, fields[0, 0], Color.White);
             game.spriteBatch.Draw(pixel, fields[0, 1], Color.White);
             game.spriteBatch.Draw(pixel, fields[0, 2], Color.White);
